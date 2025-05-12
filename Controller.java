c30185830436151c3403250c3726085ac54faf88
KhangMLe165 <khangmle@seas.upenn.edu>
2025-05-12 01:10:30 -0400

    Update Controller.java
    Create class 
    
    Update 	
    private ControllerToViewGUI myView;
    private ControllerToModel myModel;

--- Controller.java
+++ Controller.java
@@ -1,1 +1,9 @@
-
+import java.util.ArrayList;
+//manages communication between the view and the model components
+//(not REALLY needed, but could be helpful)
+public class Controller implements ViewGUIToController{
+	
+	private ControllerToViewGUI myView;
+	private ControllerToModel myModel;
+	
+	public Controller()
